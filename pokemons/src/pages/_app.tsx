0288d0aab3b4e7875828daa5c3f9518ca55c81d1
wellpinho <wellpinho@outlook.com>
2022-06-18 11:03:18 -0300

    ✨ NEW FEATURE: Next UI

--- pokemons/src/pages/_app.tsx
+++ pokemons/src/pages/_app.tsx
@@ -1,8 +1,13 @@
-import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import { NextUIProvider } from "@nextui-org/react";
+import "../../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
 
 export default MyApp;
--- pokemons/src/pages/index.tsx
+++ pokemons/src/pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>Hello world</h1>
+      <Button color="success">UI Button</Button>
     </>
   );
 };
